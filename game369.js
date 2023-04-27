835c74ed167f4696958e1b0d720325c457b7c0cc
남은지 <ejnam@assembble.com>
2023-04-27 17:36:57 +0900

    삼육구게임 업그레이드

--- game369.js
+++ game369.js
@@ -1,5 +1,19 @@
+// function 삼육구게임(num) {
+//     if ( num % 3 == 0) {
+//         console.log('박수')
+//     } else {
+//         console.log('통과');
+//     }
+// }
+
+// 삼육구게임(6);
+// 삼육구게임(12);
+// 삼육구게임(11);
+
 function 삼육구게임(num) {
-    if ( num % 3 == 0) {
+    if ( num % 9 == 0 ) {
+        console.log('박수X2');
+    } else if ( num % 3 == 0) {
         console.log('박수')
     } else {
         console.log('통과');
@@ -7,5 +21,6 @@ function 삼육구게임(num) {
 }
 
 삼육구게임(6);
-삼육구게임(12);
+삼육구게임(9);
 삼육구게임(11);
+
