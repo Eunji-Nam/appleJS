d13492369d2aff23ff5a9114e9846fd38887604e
남은지 <ejnam@assembble.com>
2023-05-02 13:07:42 +0900

    369게임/합격여부 확인하는 JS코드 만들어보기

--- game369.js
+++ game369.js
@@ -24,14 +24,45 @@
 // 삼육구게임(9);
 // 삼육구게임(11);
 
+
+// function 삼육구게임(num) {
+//     const unitDigit = num.toString()
+//     if (unitDigit[unitDigit.length-1] == 3 || unitDigit[unitDigit.length-1] == 6 || unitDigit[unitDigit.length-1] == 3) {
+//         console.log('박수')
+//     } else {
+//         console.log('통과')
+//     }
+// }
+
+// 삼육구게임(3);
+// 삼육구게임(12);
+// 삼육구게임(26);
+
+
+// function 합격했냐(개론, 민법) {
+//     if(개론 + 민법 >= 120 && 개론 >= 40 && 민법 >= 40) {
+//         console.log('합격')
+//     } else {
+//         console.log('불합격')
+//     }
+// }
+
+// 합격했냐(70, 70);
+// 합격했냐(30, 100);
+// 합격했냐(50, 50);
+
 function 합격했냐(개론, 민법) {
-    if(개론 + 민법 >= 120 && 개론 >= 40 && 민법 >= 40) {
+    if(개론 > 100 || 민법 > 100 || 개론 < 0 || 민법 < 0) {
+        console.log('장난치지마세요^^')
+    } else if(개론 + 민법 >= 120 && 개론 >= 40 && 민법 >= 40) {
         console.log('합격')
     } else {
         console.log('불합격')
     }
 }
 
+합격했냐(70, 120);
+합격했냐(-20, 90);
 합격했냐(70, 70);
 합격했냐(30, 100);
 합격했냐(50, 50);
